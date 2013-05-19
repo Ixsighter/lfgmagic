e7215bbe640407854be57c211f120b770c654c4d
Ixsighter <root@x-root.org>
2013-05-19 13:40:10 +0300

    cleaned setup file

--- setup.py
+++ setup.py
@@ -19,8 +19,6 @@ setup(
     version = '0.1.4',
     description = 'LFG Magic utility',
     author = 'Ixsighter',
-    #author_email = 'root@x-root.org',
-	#package_dir = {'': 'lib'},
     options = {'build_exe': {'excludes': excludes, 'packages': packages, 'include_files': includefiles, 'icon': "file.ico"}}, 
     executables = [Executable(script = 'LFGMagic.py', base = base, shortcutName = 'LFG Magic', shortcutDir = 'DesktopFolder')]
 )
